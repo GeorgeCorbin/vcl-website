fe7e401da0b62df8a11b3a6fef3bc1110f21b0a0
GeorgeC36 <gcorbin32@gatech.edu>
2024-03-08 15:55:04 -0800

    Fixes

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ from flask import Flask, render_template_string
 
 app = Flask(__name__)
 
+# Dummy data for votes - in a real app, this would come from a database
+votes = {
+    'team1': 49,
+    'team2': 51,
+    'team3': 75,
+    'team4': 25,
+}
+total_votes_game1 = votes['team1'] + votes['team2']
+total_votes_game2 = votes['team3'] + votes['team4']
+
 HTML_TEMPLATE = '''
 <!doctype html>
 <html lang="en">
@@ -41,6 +51,8 @@ HTML_TEMPLATE = '''
     .team {
       display: flex;
       align-items: center;
+      justify-content: space-between;
+      margin-bottom: 10px;
     }
     .team-logo {
       margin-right: 10px;
@@ -48,15 +60,19 @@ HTML_TEMPLATE = '''
     .vote-percent {
       margin-left: auto;
     }
-    .vote-button {
-      margin-left: 10px;
-      -webkit-appearance: none;
-      background-color: #f0f0f0;
-      border-radius: 50%;
-      border: none;
-      width: 20px;
-      height: 20px;
+    .select-box {
+      display: flex;
+      align-items: center;
+      justify-content: center;
       cursor: pointer;
+      width: 100px;
+      height: 100px;
+      border: 1px solid #f0f0f0;
+      margin-right: 10px;
+    }
+    .select-box img {
+      max-width: 80%;
+      max-height: 80%;
     }
     .timer {
       font-size: 0.8em;
@@ -73,38 +89,56 @@ HTML_TEMPLATE = '''
   <div class="matchup">
     <div class="timer">Closes in 18 hrs</div>
     <div class="team">
-      <div class="team-logo"><img src="/Users/georgecorbin/PycharmProjects/polling/gtlogo.png"></div>
-      <div>Team 1</div>
-      <div class="vote-percent">49%</div>
-      <input type="radio" name="team1vote" class="vote-button">
+      <label class="select-box">
+        <input type="radio" name="game1vote" style="display:none" onclick="alert('You voted for Team 1!')" />
+        <img src="/Users/georgecorbin/PycharmProjects/polling/gtlogo.png" alt="Team 1 Logo">
+      </label>
+      <div class="vote-percent">{{ team1_percent }}%</div>
     </div>
     <div class="team">
-      <div class="team-logo">[Team 2 Logo]</div>
-      <div>Team 2</div>
-      <div class="vote-percent">51%</div>
-      <input type="radio" name="team1vote" class="vote-button">
+      <label class="select-box">
+        <input type="radio" name="game1vote" style="display:none" onclick="alert('You voted for Team 2!')" />
+        <img src="path_to_team2_logo.png" alt="Team 2 Logo">
+      </label>
+      <div class="vote-percent">{{ team2_percent }}%</div>
     </div>
   </div>
   <div class="matchup">
     <div class="timer">Closes in 18 hrs</div>
     <div class="team">
-      <div class="team-logo">[Team 3 Logo]</div>
-      <div>Team 3</div>
-      <input type="radio" name="team2vote" class="vote-button">
+      <label class="select-box">
+        <input type="radio" name="game2vote" style="display:none" onclick="alert('You voted for Team 3!')" />
+        <img src="path_to_team3_logo.png" alt="Team 3 Logo">
+      </label>
+      <div class="vote-percent">{{ team3_percent }}%</div>
     </div>
     <div class="team">
-      <div class="team-logo">[Team 4 Logo]</div>
-      <div>Team 4</div>
-      <input type="radio" name="team2vote" class="vote-button">
+      <label class="select-box">
+        <input type="radio" name="game2vote" style="display:none" onclick="alert('You voted for Team 4!')" />
+        <img src="path_to_team4_logo.png" alt="Team 4 Logo">
+      </label>
+      <div class="vote-percent">{{ team4_percent }}%</div>
     </div>
   </div>
 </body>
 </html>
 '''
 
+
 @app.route('/')
 def home():
-    return render_template_string(HTML_TEMPLATE)
+    # Calculate the vote percentages
+    team1_percent = round((votes['team1'] / total_votes_game1) * 100, 1)
+    team2_percent = round((votes['team2'] / total_votes_game1) * 100, 1)
+    team3_percent = round((votes['team3'] / total_votes_game2) * 100, 1)
+    team4_percent = round((votes['team4'] / total_votes_game2) * 100, 1)
+
+    return render_template_string(HTML_TEMPLATE,
+                                  team1_percent=team1_percent,
+                                  team2_percent=team2_percent,
+                                  team3_percent=team3_percent,
+                                  team4_percent=team4_percent)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
